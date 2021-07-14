118689f23a0bb8923ee0fb537950d063e6c9c6b4
czxyy <chloe_zheng@alumni.sutd.edu.sg>
2021-07-14 21:59:11 +0800

    all required functionalities working

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const spinner = document.getElementById("spinner");
-
 let comicId; // middle comic to display
 let selectedComicNumber = 3; // onload display 3 comics
-
-let curComic;
-let lastComic;
-let prevComicId = comicId - selectedComicNumber;
-let nextComicId = comicId + selectedComicNumber;
+let loader = document.getElementById("loader");
 
 const getArrOfComicId = () => {
   let comicArr = [];
@@ -30,18 +24,26 @@ const getArrOfComicId = () => {
     comicArr = [comicId - 2, comicId - 1, comicId, comicId + 1, comicId + 2];
   }
 
-  // return evaluated arr of ids for fetch processing
+  // return newly evaluated arr of ids for fetch processing
   return comicArr.map((idArr) => evalIds(idArr));
 };
 
-async function getComic(selectedComicNumber, comicId) {
-  console.log("getComic");
-  spinner.removeAttribute("hidden");
-  spinner.setAttribute("hidden", "");
-
+async function getComic(selectedComicNumber) {
   let comicIdArr = getArrOfComicId(selectedComicNumber);
 
-  console.log("getComic", comicIdArr);
+  const appendComicAndTitle = (comicTitleDiv, comicDiv, fetchComicData) => {
+    let comicTitleId = document.querySelector(comicTitleDiv);
+    comicTitleId.innerHTML = "";
+    comicTitleId.innerHTML = fetchComicData.num + " - " + fetchComicData.title;
+
+    let comicImgDiv = document.querySelector(comicDiv);
+    comicImgDiv.innerHTML = "";
+    let comicImg = document.createElement("img");
+    comicImg.setAttribute("class", "comic-image");
+    comicImg.src = fetchComicData.img;
+    comicImg.alt = fetchComicData.alt;
+    comicImgDiv.append(comicImg);
+  };
 
   const getComicData = await Promise.all(
     comicIdArr.map(async (id) => {
@@ -51,70 +53,88 @@ async function getComic(selectedComicNumber, comicId) {
       return res.json();
     })
   );
+
   // map each comicId call into HTML element
-  getComicData.map((res) => console.log("RES", res));
+  getComicData.map((element, i) => {
+    appendComicAndTitle(
+      `#comic-title-${i + 1}`,
+      `#comic-image-${i + 1}`,
+      element
+    );
+  }, setTimeout(displayLoading, 500));
 }
 
 function displayComicSection() {
+  loader.style.display = "";
   let dropDownVal = document.getElementById("selectComicNumber").value;
   selectedComicNumber = +dropDownVal;
   let comicContent = document.getElementById("comic-content");
   comicContent.innerHTML = "";
-  console.log("selectedComicNumber", selectedComicNumber);
-  // generate 1/3/5 elements, start iterating at id = 1, to 3 and to 5 elements in the comicIdArr
+
   for (let i = 1; i < selectedComicNumber + 1; i++) {
-    let displayComics = `<div id="comic-title-${i}" class="comic-title-${i}"></div>
-    <div id="comic-image-${i}"></div>`;
-    comicContent.append(displayComics);
+    let displayComics = `<div id="comic-grid"><div id="comic-title-${i}" class="comic-title-${i}"></div>
+    <div id="comic-image-${i}" class="comic-image"></div></div>`;
+    comicContent.innerHTML += displayComics;
   }
-  getComic(selectedComicNumber, comicId);
+  getComic(selectedComicNumber);
 }
 
 const getOnLoad = () => {
   comicId = 2;
-  curComic = comicId;
-  displayComicSection();
-  getComic(selectedComicNumber, comicId);
+  displayComicSection(); // initialize HTML element
+  getComic(selectedComicNumber); // retrieve comic from api
 };
 
 const getNext = () => {
+  loader.style.display = "";
   comicId = comicId + selectedComicNumber;
-  curComic = comicId;
-  getComic(selectedComicNumber, comicId);
+  getComic(selectedComicNumber);
 };
 const getPrev = () => {
+  loader.style.display = "";
   comicId = comicId - selectedComicNumber;
-  curComic = comicId;
-  getComic(selectedComicNumber, comicId);
+  getComic(selectedComicNumber);
 };
 const getLast = () => {
+  loader.style.display = "";
   alert("last page of comic");
   comicId = 2475;
-  curComic = comicId;
-  getComic(selectedComicNumber, comicId);
+  getComic(selectedComicNumber);
 };
 
 const getFirst = () => {
+  loader.style.display = "";
   alert("go to the start of comic");
   comicId = 2;
-  curComic = comicId;
-  getComic(selectedComicNumber, comicId);
+  getComic(selectedComicNumber);
 };
 
 const getRandom = () => {
+  loader.style.display = "";
   comicId = Math.floor(Math.random() * 2475);
-  curComic = comicId;
-  getComic(selectedComicNumber, comicId);
+  getComic(selectedComicNumber);
 };
 
 const getSearch = () => {
+  loader.style.display = "";
   const searchInputValue = document.getElementById("searchInput").value;
   if (searchInputValue === "") {
     return;
+  } else if (
+    searchInputValue > 2475 ||
+    isNaN(searchInputValue) ||
+    searchInputValue < 1
+  ) {
+    alert("invalid comic #");
+    loader.style.display = "none";
+  } else {
+    comicId = +searchInputValue;
+    getComic(selectedComicNumber);
   }
-  comicId = +searchInputValue;
-  curComic = +comicId;
-  getComic(selectedComicNumber, comicId);
+};
+
+const displayLoading = () => {
+  loader.style.display = "none";
 };
 
 // handle button click event
